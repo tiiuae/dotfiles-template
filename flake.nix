e5fb198a66b978461ea664f16248a0abb9741ad8
Brian McGillion <bmg.avoin@gmail.com>
2023-04-11 22:04:19 +0400

    Tidy up
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- flake.nix
+++ flake.nix
@@ -25,8 +25,10 @@
     };
 
     # TODO should we just follow main (instead of the tag)
-    alejandra.url = "github:kamadorueda/alejandra/3.0.0";
-    alejandra.inputs.nixpkgs.follows = "nixpkgs";
+    alejandra = {
+      url = "github:kamadorueda/alejandra/3.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # TODO add nixos-generators
   };
