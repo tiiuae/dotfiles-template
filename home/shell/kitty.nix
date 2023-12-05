fb2fc54bdb72cad53de63110185a6ef905bf95bc
Brian McGillion <bmg.avoin@gmail.com>
2023-12-05 06:30:02 +0400

    some kitty changes
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- home/shell/kitty.nix
+++ home/shell/kitty.nix
@@ -8,6 +8,14 @@
       };
       shellIntegration.enableBashIntegration = true;
       theme = "Dracula";
+      settings = {
+        enable_audio_bell = false;
+        enabled_layouts = "grid,stack,horizontal,tall";
+      };
+      extraConfig = "
+          map ctrl+alt+g goto_layout grid
+          map ctrl+alt+x goto_layout stack
+";
     };
   };
 }
