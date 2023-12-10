336bfccba8574ff7273d82f045a87e844ff2fcf9
Brian McGillion <bmg.avoin@gmail.com>
2023-12-10 07:23:12 +0400

    Disable root login and explicit calling of ecdsa keys
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- hosts/common-server.nix
+++ hosts/common-server.nix
@@ -3,6 +3,7 @@
   self,
   inputs,
   config,
+  pkgs,
   ...
 }: {
   imports = [
@@ -16,13 +17,23 @@
     setup.device.isServer = true;
 
     environment.systemPackages = [
+      pkgs.kitty.terminfo
     ];
 
     services.openssh = {
       enable = true;
       settings = {
+        PermitRootLogin = lib.mkForce "no";
         PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        ClientAliveInterval = lib.mkDefault 60;
       };
+      hostKeys = [
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
     };
   };
 }
