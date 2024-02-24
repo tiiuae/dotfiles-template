f4ddc4b3863b2297b7c22041e48fb7a058788dcf
Brian McGillion <bmg.avoin@gmail.com>
2024-02-24 18:25:09 +0400

    Add a reference for file scripts
    Signed-off-by: Brian McGillion <bmg.avoin@gmail.com>

--- packages/scripts/default.nix
+++ packages/scripts/default.nix
@@ -8,10 +8,13 @@
   rebuild-x1 = pkgs.writeScriptBin "rebuild-x1" ''
     nixos-rebuild --flake .#lenovo-x1-carbon-gen11-debug --target-host "root@ghaf-host" --fast switch
   '';
+  #https://discourse.nixos.org/t/install-shell-script-on-nixos/6849/10
+  #ownfile = pkgs.callPackage ./ownfile.nix {};
 in {
   environment.systemPackages = with pkgs; [
     rebuild-arcadia
     rebuild-nephele
     rebuild-x1
+    #ownfile
   ];
 }
